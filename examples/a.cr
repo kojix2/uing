c4cd2e84b5ab8b4ea6c91fd889c6d4d76f73df37
kojix2 <2xijok@gmail.com>
2024-04-27 13:19:09 +0900

    Add very primitive example

--- examples/a.cr
+++ examples/a.cr
@@ -5,5 +5,11 @@ LibUI::LibUI.init(Pointer(LibUI::LibUI::InitOptions).malloc)
 w = LibUI::LibUI.new_window("Hello", 300, 200, 1)
 LibUI::LibUI.control_show(w)
 
+f = ->(a : Pointer(Void), b : Pointer(Void)) {
+  LibUI::LibUI.quit
+  1
+}
+LibUI::LibUI.window_on_closing(w, f, nil)
+
 LibUI::LibUI.main
 LibUI::LibUI.uninit
