621a8644ddf8426494c3fa38b05ef5be404b9b21
kojix2 <2xijok@gmail.com>
2025-01-20 16:09:15 +0900

    Add Windows SDK to PATH and update linker flags for Windows manifest

--- src/uing/lib_ui/lib_ui.cr
+++ src/uing/lib_ui/lib_ui.cr
@@ -10,7 +10,7 @@ module UIng
     @[Link("D2d1")]
     @[Link("Windowscodecs")]
     @[Link("#{__DIR__}/../../../libui")]
-    @[Link(ldflags: "/SUBSYSTEM:WINDOWS")]
+    @[Link(ldflags: "/SUBSYSTEM:WINDOWS /MANIFEST /MANIFEST:EMBED /MANIFESTDEPENDENCY:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' language='*'\"")]
   {% elsif flag?(:linux) %}
     @[Link("gtk+-3.0")]
     @[Link("m")]
