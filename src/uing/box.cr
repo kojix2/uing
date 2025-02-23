1e59d6874dc25d24fb80b03e46d80e87e7f2650d
kojix2 <2xijok@gmail.com>
2025-02-23 14:27:29 +0900

    Fix Box class initialization by separating VerticalBox and HorizontalBox

--- src/uing/box.cr
+++ src/uing/box.cr
@@ -2,18 +2,29 @@ require "./control"
 
 module UIng
   # Note: The name Box is already taken by Crystal's built-in class Box.
-  class Box
+  abstract class Box
     include Control
-
-    def initialize(@ref_ptr : Pointer(LibUI::Box))
-    end
-
-    # def initialize
-    #   @ref_ptr = LibUI.new_box
-    # end
 
     def to_unsafe
       @ref_ptr
+    end
+  end
+
+  class VerticalBox < Box
+    def initialize(@ref_ptr : Pointer(LibUI::Box))
+    end
+
+    def initialize
+      @ref_ptr = LibUI.new_vertical_box
+    end
+  end
+
+  class HorizontalBox < Box
+    def initialize(@ref_ptr : Pointer(LibUI::Box))
+    end
+
+    def initialize
+      @ref_ptr = LibUI.new_horizontal_box
     end
   end
 end
