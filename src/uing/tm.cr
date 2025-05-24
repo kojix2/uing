cde2f894f25e69b4252255aa198d5f33cb5a69db
kojix2 <2xijok@gmail.com>
2025-05-24 19:49:58 +0900

    Refactor TM class to improve constructor clarity and add time conversion methods

--- src/uing/tm.cr
+++ src/uing/tm.cr
@@ -1,6 +1,24 @@
 module UIng
   class TM
+    # Default constructor
     def initialize(@cstruct : LibUI::TM = LibUI::TM.new)
+    end
+
+    # Overloaded constructor: Convert Time to TM
+    def initialize(time : ::Time)
+      @cstruct = LibUI::TM.new
+      self.year = time.year
+      self.mon = time.month
+      self.mday = time.day
+      self.hour = time.hour
+      self.min = time.minute
+      self.sec = time.second
+      self.wday = time.day_of_week.to_i % 7  # 0 = Sunday
+      self.yday = time.day_of_year - 1       # 0-based
+      self.isdst = 0 # Not handling DST
+      {% unless flag?(:windows) %}
+        self.zone = time.location.name
+      {% end %}
     end
 
     {% unless flag?(:windows) %}
@@ -18,6 +36,18 @@ module UIng
 
     def to_unsafe
       pointerof(@cstruct)
+    end
+
+    # Convert TM to Time
+    def to_time : ::Time
+      ::Time.local(
+        self.year,
+        self.mon,
+        self.mday,
+        self.hour,
+        self.min,
+        self.sec
+      )
     end
   end
 end
