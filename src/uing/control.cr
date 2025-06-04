37367b76d2c95675c8b19f9c7d2880b25602ad2b
kojix2 <2xijok@gmail.com>
2025-06-04 09:06:22 +0900

    Remove MethodMissing form Control

--- src/uing/control.cr
+++ src/uing/control.cr
@@ -53,67 +53,62 @@ module UIng
   end
 
   module Control
-    include MethodMissing
     include BlockConstructor
 
-    def destroy
-      UIng.control_destroy(@ref_ptr)
+    def destroy : Nil
+      LibUI.control_destroy(UIng.to_control(@ref_ptr))
     end
 
     def handle
-      UIng.control_handle(@ref_ptr)
+      LibUI.control_handle(UIng.to_control(@ref_ptr))
     end
 
     def parent
-      UIng.control_parent(@ref_ptr)
+      LibUI.control_parent(UIng.to_control(@ref_ptr))
     end
 
-    def set_parent(parent)
-      UIng.control_set_parent(@ref_ptr, parent)
+    def set_parent(parent) : Nil
+      LibUI.control_set_parent(UIng.to_control(@ref_ptr), UIng.to_control(parent))
     end
 
-    def parent=(parent)
+    def parent=(parent) : Nil
       set_parent(parent)
     end
 
-    def toplevel
-      UIng.control_toplevel(@ref_ptr)
+    def toplevel? : Bool
+      LibUI.control_toplevel(UIng.to_control(@ref_ptr))
     end
 
-    def toplevel?
-      toplevel
+    def visible? : Bool
+      LibUI.control_visible(UIng.to_control(@ref_ptr))
     end
 
-    def visible : Bool
-      UIng.control_visible(@ref_ptr)
+    def show : Nil
+      LibUI.control_show(UIng.to_control(@ref_ptr))
     end
 
-    def visible?
-      visible
+    def hide : Nil
+      LibUI.control_hide(UIng.to_control(@ref_ptr))
     end
 
-    def show
-      UIng.control_show(@ref_ptr)
+    def enabled? : Bool
+      LibUI.control_enabled(UIng.to_control(@ref_ptr))
     end
 
-    def hide
-      UIng.control_hide(@ref_ptr)
+    def enable : Nil
+      LibUI.control_enable(UIng.to_control(@ref_ptr))
     end
 
-    def enabled : Bool
-      UIng.control_enabled(@ref_ptr)
+    def disable : Nil
+      LibUI.control_disable(UIng.to_control(@ref_ptr))
     end
 
-    def enabled?
-      enabled
+    def enabled_to_user? : Bool
+      LibUI.control_enabled_to_user(UIng.to_control(@ref_ptr))
     end
 
-    def enable : Bool
-      UIng.control_enable(@ref_ptr)
-    end
-
-    def disable
-      UIng.control_disable(@ref_ptr)
+    def verify_set_parent(parent) : Nil
+      LibUI.control_verify_set_parent(UIng.to_control(@ref_ptr), UIng.to_control(parent))
     end
   end
 end
