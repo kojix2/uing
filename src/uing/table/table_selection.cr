eede21a849d32c436d5a628d39b4fc5f8c2fe33b
kojix2 <2xijok@gmail.com>
2025-06-04 19:41:05 +0900

    Add managed_by_libui property to TableSelection and update initialization logic

--- src/uing/table/table_selection.cr
+++ src/uing/table/table_selection.cr
@@ -1,10 +1,14 @@
 module UIng
   class TableSelection
+    property? managed_by_libui : Bool = false
+
     def initialize(@cstruct : LibUI::TableSelection = LibUI::TableSelection.new)
+      @managed_by_libui = false  # TableSelection created by ourselves
     end
 
     def initialize(ptr : Pointer(LibUI::TableSelection))
       @cstruct = ptr.value
+      @managed_by_libui = true  # TableSelection managed by LibUI
     end
 
     def num_rows : Int32
@@ -16,11 +20,19 @@ module UIng
     end
 
     def free : Nil
+      return if @managed_by_libui  # Don't free TableSelection managed by LibUI
       LibUI.free_table_selection(self.to_unsafe)
     end
 
     def to_unsafe
       pointerof(@cstruct)
     end
+
+    # Note: No finalize method needed for TableSelection
+    # According to libui-ng developers:
+    # - TableSelection objects returned from libui-ng are managed internally
+    # - Language bindings should treat these as "borrowed" references that don't need cleanup
+    # - libui-ng uses strict ownership model where it manages TableSelection memory internally
+    # - Adding finalize would cause double-free errors since libui-ng already manages the memory
   end
 end
