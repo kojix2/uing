2fe489f95e99b1b2cb3f457733b6d99e287fab53
kojix2 <2xijok@gmail.com>
2025-06-08 17:36:56 +0900

    Refactor TableValue class

--- src/uing/table/table_value.cr
+++ src/uing/table/table_value.cr
@@ -1,50 +1,64 @@
 module UIng
   # TableValue represents data values in Table cells.
   #
-  # CRITICAL MEMORY MANAGEMENT WARNINGS:
-  # 1. TableValue returned from TableModelHandler callbacks is "borrowed" - DO NOT free
+  # UNIFIED MEMORY MANAGEMENT:
+  # TableValue uses a borrowed flag to automatically handle memory management.
+  #
+  # CRITICAL WARNINGS:
+  # 1. TableValue from TableModelHandler callbacks is "borrowed" - automatically protected from free
   # 2. TableValue created by Crystal code MUST be freed after use
-  # 3. DO NOT store TableValue references long-term - use immediately and free
+  # 3. DO NOT store TableValue references long-term
   # 4. libui-ng takes ownership when TableValue is passed to Table methods
+  # 5. Image data returned from TableValue.image is borrowed - DO NOT free
+  # 6. String data returned from TableValue.string is borrowed - DO NOT free
   #
   # Safe usage patterns:
-  #   # In TableModelHandler callback (borrowed):
+  #   # In TableModelHandler callback (borrowed - automatically protected):
   #   def cell_value(...)
   #     value = LibUI.new_table_value_string("data")
   #     return value  # libui-ng will free this
   #   end
   #
-  #   # Creating for immediate use:
+  #   # Creating for immediate use (must free):
   #   value = TableValue.new("data")
-  #   # use value immediately
-  #   value.free  # free when done
+  #   data = value.string  # Extract data immediately
+  #   value.free           # Free immediately after use
+  #
+  #   # Reading from borrowed TableValue (automatically protected):
+  #   def set_cell_value(row, column, value_ptr)
+  #     table_value = TableValue.new(value_ptr, borrowed: true)  # Marked as borrowed
+  #     data = table_value.string                               # Extract data
+  #     table_value.free  # Safe to call - will be ignored for borrowed values
+  #   end
   class TableValue
     property? released : Bool = false
-    property? managed_by_libui : Bool = false
+    property? borrowed : Bool = false
 
-    def initialize(@ref_ptr : Pointer(LibUI::TableValue))
-      @managed_by_libui = true # TableValue managed by LibUI - DO NOT free
+    # Unified constructor - handles both borrowed and owned TableValue
+    def initialize(@ref_ptr : Pointer(LibUI::TableValue), borrowed : Bool = false)
+      @borrowed = borrowed
     end
 
-    # Overloaded initializers for convenient creation
+    # Public constructors for creating new TableValue objects
+    # These MUST be freed after use
     def initialize(str : String)
       @ref_ptr = LibUI.new_table_value_string(str)
-      @managed_by_libui = false # TableValue created by ourselves
+      @borrowed = false
     end
 
     def initialize(image : Image)
       @ref_ptr = LibUI.new_table_value_image(image.to_unsafe)
-      @managed_by_libui = false
+      @borrowed = false
     end
 
     def initialize(i : Int32)
       @ref_ptr = LibUI.new_table_value_int(i)
-      @managed_by_libui = false
+      @borrowed = false
     end
 
     def initialize(r : Float64, g : Float64, b : Float64, a : Float64)
       @ref_ptr = LibUI.new_table_value_color(r, g, b, a)
-      @managed_by_libui = false
+      @borrowed = false
     end
 
     # def initialize
@@ -57,7 +71,7 @@ module UIng
 
     def free : Nil
       return if @released
-      return if @managed_by_libui # Don't free TableValue managed by LibUI
+      return if @borrowed # Don't free borrowed TableValue
       LibUI.free_table_value(@ref_ptr)
       @released = true
     end
@@ -68,12 +82,15 @@ module UIng
 
     def string : String?
       str_ptr = LibUI.table_value_string(@ref_ptr)
-      UIng.string_from_pointer(str_ptr)
+      return nil if str_ptr.null?
+      # DO NOT free the string pointer - it's borrowed from libui-ng
+      String.new(str_ptr)
     end
 
-    def image : Image
-      ref_ptr = LibUI.table_value_image(@ref_ptr)
-      Image.new(ref_ptr)
+    def image : Pointer(LibUI::Image)
+      # Return the raw pointer - DO NOT create new Image object
+      # The returned pointer is borrowed from libui-ng and should not be freed
+      LibUI.table_value_image(@ref_ptr)
     end
 
     def int : Int32
