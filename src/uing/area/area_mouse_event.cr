8da26491b4568a0b4cc1833717df22bdd5e41da8
kojix2 <2xijok@gmail.com>
2025-06-12 20:05:50 +0900

    Remove forward_missing_to and explicitly define accessors for AreaMouseEvent properties

--- src/uing/area/area_mouse_event.cr
+++ src/uing/area/area_mouse_event.cr
@@ -3,7 +3,41 @@ module UIng
     def initialize(@cstruct : LibUI::AreaMouseEvent = LibUI::AreaMouseEvent.new)
     end
 
-    forward_missing_to(@cstruct)
+    def x
+      @cstruct.x
+    end
+
+    def y
+      @cstruct.y
+    end
+
+    def area_width
+      @cstruct.area_width
+    end
+
+    def area_height
+      @cstruct.area_height
+    end
+
+    def down
+      @cstruct.down
+    end
+
+    def up
+      @cstruct.up
+    end
+
+    def count
+      @cstruct.count
+    end
+
+    def modifiers
+      @cstruct.modifiers
+    end
+
+    def held1_to64
+      @cstruct.held1_to64
+    end
 
     def to_unsafe
       pointerof(@cstruct)
