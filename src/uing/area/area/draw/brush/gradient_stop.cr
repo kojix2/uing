973577ad6f90948cdde83ee2bd56b8fbd682f6a6
kojix2 <2xijok@gmail.com>
2025-06-21 07:53:27 +0900

    Remove forward_missing to in StrokeParams and GradientStop

--- src/uing/area/area/draw/brush/gradient_stop.cr
+++ src/uing/area/area/draw/brush/gradient_stop.cr
@@ -3,10 +3,49 @@ module UIng
     module Draw
       class Brush
         class GradientStop
-          def initialize(@cstruct : LibUI::DrawBrushGradientStop = LibUI::DrawBrushGradientStop.new)
+          def initialize
+            @cstruct = LibUI::DrawBrushGradientStop.new
           end
 
-          forward_missing_to(@cstruct)
+          def pos : Float64
+            @cstruct.pos
+          end
+
+          def pos=(value : Number)
+            @cstruct.pos = value.to_f64
+          end
+
+          def r : Float64
+            @cstruct.r
+          end
+
+          def r=(value : Number)
+            @cstruct.r = value.to_f64
+          end
+
+          def g : Float64
+            @cstruct.g
+          end
+
+          def g=(value : Number)
+            @cstruct.g = value.to_f64
+          end
+
+          def b : Float64
+            @cstruct.b
+          end
+
+          def b=(value : Number)
+            @cstruct.b = value.to_f64
+          end
+
+          def a : Float64
+            @cstruct.a
+          end
+
+          def a=(value : Number)
+            @cstruct.a = value.to_f64
+          end
 
           def to_unsafe
             pointerof(@cstruct)
--- src/uing/area/area/draw/stroke_params.cr
+++ src/uing/area/area/draw/stroke_params.cr
@@ -2,12 +2,83 @@ module UIng
   class Area < Control
     module Draw
       class StrokeParams
-        def initialize(@cstruct : LibUI::DrawStrokeParams = LibUI::DrawStrokeParams.new)
+        def initialize
+          @cstruct = LibUI::DrawStrokeParams.new
+          @dashes_array = Array(Float64).new
         end
 
-        forward_missing_to(@cstruct)
+        # Basic properties with direct delegation
+        def cap : UIng::Area::Draw::LineCap
+          @cstruct.cap
+        end
+
+        def cap=(value : UIng::Area::Draw::LineCap)
+          @cstruct.cap = value
+        end
+
+        def join : UIng::Area::Draw::LineJoin
+          @cstruct.join
+        end
+
+        def join=(value : UIng::Area::Draw::LineJoin)
+          @cstruct.join = value
+        end
+
+        def thickness : Float64
+          @cstruct.thickness
+        end
+
+        def thickness=(value : Number)
+          @cstruct.thickness = value.to_f64
+        end
+
+        def miter_limit : Float64
+          @cstruct.miter_limit
+        end
+
+        def miter_limit=(value : Number)
+          @cstruct.miter_limit = value.to_f64
+        end
+
+        def dash_phase : Float64
+          @cstruct.dash_phase
+        end
+
+        def dash_phase=(value : Number)
+          @cstruct.dash_phase = value.to_f64
+        end
+
+        # Dashes property using Crystal Array
+        def dashes : Array(Float64)
+          @dashes_array
+        end
+
+        def dashes=(values : Array(Float64))
+          @dashes_array = values
+          sync_dashes
+        end
+
+        def dashes=(values : Enumerable(Float64))
+          @dashes_array = values.to_a
+          sync_dashes
+        end
+
+        def num_dashes : Int32
+          @dashes_array.size
+        end
+
+        private def sync_dashes
+          if @dashes_array.empty?
+            @cstruct.dashes = Pointer(LibC::Double).null
+            @cstruct.num_dashes = 0_u64
+          else
+            @cstruct.dashes = @dashes_array.to_unsafe.as(Pointer(LibC::Double))
+            @cstruct.num_dashes = @dashes_array.size.to_u64
+          end
+        end
 
         def to_unsafe
+          sync_dashes
           pointerof(@cstruct)
         end
       end
