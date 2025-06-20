c4b5d9a4088ed8fff2e52e95da6d7c136b3a9034
kojix2 <2xijok@gmail.com>
2025-06-20 22:53:40 +0900

    Move ExtKey enum reference to Area class

--- src/uing/lib_ui/area_key_event.cr
+++ src/uing/lib_ui/area_key_event.cr
@@ -2,7 +2,7 @@ module UIng
   lib LibUI
     struct AreaKeyEvent
       key : LibC::Char
-      ext_key : ExtKey
+      ext_key : UIng::Area::ExtKey
       modifier : Modifiers
       modifiers : Modifiers
       up : LibC::Int
