ed8d04ae78c5fe126fb1811e5be025e79cb13bf3
kojix2 <2xijok@gmail.com>
2025-08-16 10:00:57 +0900

    Update download.cr

--- download.cr
+++ download.cr
@@ -1,7 +1,7 @@
 require "compress/zip"
 require "file_utils"
 
-COMMIT_HASH = ENV["LIBUI_NG_COMMIT_HASH"]? || "1fd9c4"
+COMMIT_HASH = ENV["LIBUI_NG_COMMIT_HASH"]? || "8347960"
 
 # Path constants
 BUILD_DIR      = "builddir"
