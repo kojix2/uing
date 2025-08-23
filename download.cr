baa802692c22a7e5a0a4005919059cf65be7eec2
kojix2 <2xijok@gmail.com>
2025-08-23 21:40:49 +0900

    Update libui-ng version

--- download.cr
+++ download.cr
@@ -1,7 +1,7 @@
 require "compress/zip"
 require "file_utils"
 
-COMMIT_HASH = ENV["LIBUI_NG_COMMIT_HASH"]? || "c915e6e-experimental"
+COMMIT_HASH = ENV["LIBUI_NG_COMMIT_HASH"]? || "5abbd6f-experimental"
 
 # Path constants
 BUILD_DIR      = "builddir"
