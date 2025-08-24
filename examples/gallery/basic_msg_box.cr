1ba06a7f570d915d3ddf25ef4b426d4e8c670719
kojix2 <2xijok@gmail.com>
2025-08-25 01:14:44 +0900

    Simplify msg_box examples

--- examples/gallery/basic_msg_box.cr
+++ examples/gallery/basic_msg_box.cr
@@ -2,14 +2,6 @@ require "../../src/uing"
 
 UIng.init
 
-window = UIng::Window.new("MsgBox Examples", 300, 200)
-window.on_closing do
-  UIng.quit
-  true
-end
-window.show
+UIng.msg_box("Message", "Hello Crystal World!")
 
-window.msg_box("Message", "Hello Crystal World!")
-
-UIng.main
 UIng.uninit
--- examples/gallery/basic_msg_box_error.cr
+++ examples/gallery/basic_msg_box_error.cr
@@ -2,14 +2,6 @@ require "../../src/uing"
 
 UIng.init
 
-window = UIng::Window.new("MsgBoxError Examples", 300, 200)
-window.on_closing do
-  UIng.quit
-  true
-end
-window.show
+UIng.msg_box_error("Error", "An error occurred.")
 
-window.msg_box_error("Error", "An error occurred.")
-
-UIng.main
 UIng.uninit
