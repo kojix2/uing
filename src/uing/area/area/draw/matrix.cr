3d48e2c5fc65485dd51506213a6ed06207c00dc2
kojix2 <2xijok@gmail.com>
2026-03-10 00:18:43 +0900

    Refactor matrix methods to simplify 'to_unsafe' calls

--- src/uing/area/area/draw/matrix.cr
+++ src/uing/area/area/draw/matrix.cr
@@ -9,54 +9,54 @@ module UIng
         end
 
         def set_identity : self
-          LibUI.draw_matrix_set_identity(self.to_unsafe)
+          LibUI.draw_matrix_set_identity(to_unsafe)
           self
         end
 
         def translate(x : Float64, y : Float64) : self
-          LibUI.draw_matrix_translate(self.to_unsafe, x, y)
+          LibUI.draw_matrix_translate(to_unsafe, x, y)
           self
         end
 
         def scale(x_center : Float64, y_center : Float64, x : Float64, y : Float64) : self
-          LibUI.draw_matrix_scale(self.to_unsafe, x_center, y_center, x, y)
+          LibUI.draw_matrix_scale(to_unsafe, x_center, y_center, x, y)
           self
         end
 
         def rotate(x : Float64, y : Float64, amount : Float64) : self
-          LibUI.draw_matrix_rotate(self.to_unsafe, x, y, amount)
+          LibUI.draw_matrix_rotate(to_unsafe, x, y, amount)
           self
         end
 
         def skew(x : Float64, y : Float64, x_amount : Float64, y_amount : Float64) : self
-          LibUI.draw_matrix_skew(self.to_unsafe, x, y, x_amount, y_amount)
+          LibUI.draw_matrix_skew(to_unsafe, x, y, x_amount, y_amount)
           self
         end
 
         def multiply(src : Matrix) : self
-          LibUI.draw_matrix_multiply(self.to_unsafe, src.to_unsafe)
+          LibUI.draw_matrix_multiply(to_unsafe, src.to_unsafe)
           self
         end
 
         def invertible? : Bool
-          LibUI.draw_matrix_invertible(self.to_unsafe)
+          LibUI.draw_matrix_invertible(to_unsafe)
         end
 
         def invert : Bool
-          LibUI.draw_matrix_invert(self.to_unsafe)
+          LibUI.draw_matrix_invert(to_unsafe)
         end
 
         def transform_point(x : Float64, y : Float64) : {Float64, Float64}
           x2 = x
           y2 = y
-          LibUI.draw_matrix_transform_point(self.to_unsafe, pointerof(x2), pointerof(y2))
+          LibUI.draw_matrix_transform_point(to_unsafe, pointerof(x2), pointerof(y2))
           {x2, y2}
         end
 
         def transform_size(x : Float64, y : Float64) : {Float64, Float64}
           x2 = x
           y2 = y
-          LibUI.draw_matrix_transform_size(self.to_unsafe, pointerof(x2), pointerof(y2))
+          LibUI.draw_matrix_transform_size(to_unsafe, pointerof(x2), pointerof(y2))
           {x2, y2}
         end
 
