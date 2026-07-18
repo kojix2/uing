cb654b49b34e800ca4d56d4b46a512a626f71a68
kojix2 <2xijok@gmail.com>
2026-07-18 22:00:52 +0900

    crystal tool format

--- examples/gallery/basic_draw_text.cr
+++ examples/gallery/basic_draw_text.cr
@@ -9,30 +9,30 @@ title = "Michael Ende (1929-1995) The Neverending Story"
 
 str1 = \
    "  At last Ygramul sensed that something was coming toward " \
- "her. With the speed of lightning, she turned about, confronting " \
- "Atreyu with an enormous steel-blue face. Her single eye had a " \
- "vertical pupil, which stared at Atreyu with inconceivable malignancy. "
+   "her. With the speed of lightning, she turned about, confronting " \
+   "Atreyu with an enormous steel-blue face. Her single eye had a " \
+   "vertical pupil, which stared at Atreyu with inconceivable malignancy. "
 
 str2 = \
    "  A cry of fear escaped Bastian. "
 
 str3 = \
    "  A cry of terror passed through the ravine and echoed from " \
- "side to side. Ygramul turned her eye to left and right, to see if " \
- "someone else had arrived, for that sound could not have been " \
- "made by the boy who stood there as though paralyzed with " \
- "horror. "
+   "side to side. Ygramul turned her eye to left and right, to see if " \
+   "someone else had arrived, for that sound could not have been " \
+   "made by the boy who stood there as though paralyzed with " \
+   "horror. "
 
 str4 = \
    "  Could she have heard my cry? Bastion wondered in alarm. " \
- "But that's not possible. "
+   "But that's not possible. "
 
 str5 = \
    "  And then Atreyu heard Ygramuls voice. It was very high " \
- "and slightly hoarse, not at all the right kind of voice for that " \
- "enormous face. Her lips did not move as she spoke. It was the " \
- "buzzing of a great swarm of hornets that shaped itself into " \
- "words. "
+   "and slightly hoarse, not at all the right kind of voice for that " \
+   "enormous face. Her lips did not move as she spoke. It was the " \
+   "buzzing of a great swarm of hornets that shaped itself into " \
+   "words. "
 
 ATTR_STR = UIng::Area::AttributedString.new("")
 
