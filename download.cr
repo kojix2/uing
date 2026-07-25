d28d5f035bcac1862527b1c70c859016efd4419a
kojix2 <2xijok@gmail.com>
2026-07-25 16:56:14 +0900

    Remove space

--- download.cr
+++ download.cr
@@ -1,7 +1,7 @@
 require "compress/zip"
 require "file_utils"
 
-COMMIT_HASH = ENV["LIBUI_NG_COMMIT_HASH"]? || "1ef1513-experimental "
+COMMIT_HASH = ENV["LIBUI_NG_COMMIT_HASH"]? || "1ef1513-experimental"
 
 # Path constants
 BUILD_DIR      = "builddir"
